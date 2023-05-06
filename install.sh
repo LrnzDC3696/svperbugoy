1c99390d13b64c1fea0c3ba7f1afb094138bba3d
LrnzdDC3696 <lrnzdc3696@outlook.com>
2023-05-06 23:53:04 +0800

    added extra features isntall.sh

--- install.sh
+++ install.sh
@@ -12,24 +12,12 @@ fi
 # Updating and Upgrading the system
 apt-get update 
 apt-get upgrade
+apt-get install net-tools ufw -y
 ########################################################################################
 
 ########################################################################################
-# Disabling the GUI
-systemctl set-default multi-user.target
-########################################################################################
-
-########################################################################################
-echo "" >> /etc/ssh/sshd_config
-echo "# This was added from the sshd_config_file" >> /etc/ssh/sshd_config
-echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
-echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
-echo "SSH enabled"
-########################################################################################
-
-########################################################################################
-# Installing docker
-apt-get install ca-certificates curl gnupg
+# https://docs.docker.com/engine/install/debian/
+apt-get install ca-certificates curl gnupg -y
 
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -39,4 +27,45 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
+########################################################################################
+
+########################################################################################
+echo "" >> /etc/sudoers
+echo "# This was added from the install.sh" >> /etc/sudoers
+echo "bugoy    ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "Added sudoers"
+########################################################################################
+
+########################################################################################
+echo "" >> /etc/systemd/logind.conf
+echo "# This was added from the install.sh" >> /etc/systemd/logind.conf
+echo "HandleLidSwitch=ignore" >> /etc/systemd/logind.conf
+echo "HandleLidSwitchExternalPower=ignore" >> /etc/systemd/logind.conf
+echo "HandleLidSwitchDocked=ignore" >> /etc/systemd/logind.conf
+echo "Disabled laptop close lid"
+########################################################################################
+
+########################################################################################
+echo "" >> /etc/ssh/sshd_config
+echo "# This was added from the install.sh" >> /etc/ssh/sshd_config
+echo "PubkeyAuthentication yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+echo "SSH enabled"
+########################################################################################
+
+########################################################################################
+# Disabling the GUI
+systemctl set-default multi-user.target
+########################################################################################
+
+########################################################################################
+# Firewall Stuff
+# apt install ufw
+ufw allow 22
+########################################################################################
+
+
+########################################################################################
+> .bashrc
+echo "" >> .bashrc
 ########################################################################################
